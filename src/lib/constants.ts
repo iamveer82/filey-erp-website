5872488c96c97a9ddbafcd88636b1a62038a63a4
iamveer82 <iamveer82@gmail.com>
2026-07-22 17:23:51 +0400

    chore: update download links to v2.3.10

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,17 +5,17 @@ export const LICENSE_URL = `${REPO_URL}/blob/main/LICENSE`
 export const CONTRIBUTING_URL = `${REPO_URL}/blob/main/CONTRIBUTING.md`
 export const ROADMAP_URL = `${REPO_URL}/blob/main/ROADMAP.md`
 
-export const APP_VERSION = '2.3.9'
+export const APP_VERSION = '2.3.10'
 export const RELEASE_TAG = `v${APP_VERSION}`
 export const RELEASE_CODENAME = 'Latest'
 
 const DL = `${RELEASES_URL}/download/${RELEASE_TAG}`
 export const DOWNLOAD_URLS = {
-  windowsExe: `${DL}/Filey.ERP_2.3.9_x64-setup.exe`,
-  windowsMsi: `${DL}/Filey.ERP_2.3.9_x64_en-US.msi`,
-  linuxAppImage: `${DL}/Filey.ERP_2.3.9_amd64.AppImage`,
-  linuxDeb: `${DL}/Filey.ERP_2.3.9_amd64.deb`,
-  linuxRpm: `${DL}/Filey.ERP-2.3.9-1.x86_64.rpm`,
+  windowsExe: `${DL}/Filey.ERP_2.3.10_x64-setup.exe`,
+  windowsMsi: `${DL}/Filey.ERP_2.3.10_x64_en-US.msi`,
+  linuxAppImage: `${DL}/Filey.ERP_2.3.10_amd64.AppImage`,
+  linuxDeb: `${DL}/Filey.ERP_2.3.10_amd64.deb`,
+  linuxRpm: `${DL}/Filey.ERP-2.3.10-1.x86_64.rpm`,
 } as const
 
 export type OS = 'windows' | 'macos' | 'linux'
